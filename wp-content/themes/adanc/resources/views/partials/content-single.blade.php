9ec3f10be280ae6e6afae871f4972ccaab7277f6
lexinamer <lexinamer@gmail.com>
2018-08-03 14:49:39 -0400

    cleaning up blog page

--- wp-content/themes/adanc/resources/views/partials/content-single.blade.php
+++ wp-content/themes/adanc/resources/views/partials/content-single.blade.php
@@ -2,9 +2,15 @@
   <header>
     <h1 class="entry-title">{{ get_the_title() }}</h1>
     <!-- @include('partials/entry-meta') -->
+    <img class="post-image" src="{!! get_the_post_thumbnail_url($id, 'medium') !!}"/>
   </header>
   <div class="entry-content">
     @php the_content() @endphp
+
+    <div class="nextposts">
+      @php previous_post_link( '%link', '<= Previous Post ', TRUE ) @endphp
+      @php next_post_link( '%link', 'Next Post =>', TRUE ) @endphp
+    </div>
   </div>
-  @php comments_template('/partials/comments.blade.php') @endphp
+  <!-- @php comments_template('/partials/comments.blade.php') @endphp -->
 </article>
--- wp-content/themes/adanc/resources/views/partials/content.blade.php
+++ wp-content/themes/adanc/resources/views/partials/content.blade.php
@@ -1,6 +1,10 @@
 <article class="col s12 m6 l4" @php post_class() @endphp>
   <header>
-    <img src="{!! get_the_post_thumbnail_url($id, 'medium') !!}"/>
+    <div class="image-container">
+    @if(has_post_thumbnail())
+      <img src="{!! get_the_post_thumbnail_url($id, 'medium') !!}"/>
+    @endif
+    </div>
     <h3 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h3>
     <!-- @include('partials/entry-meta') -->
   </header>
