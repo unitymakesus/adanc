6bbe6e779118a143b9fb523ab45c417649161051
lexinamer <lexinamer@gmail.com>
2018-08-06 14:05:05 -0400

    Fixing services layout

--- wp-content/themes/adanc/resources/views/archive-services.blade.php
+++ wp-content/themes/adanc/resources/views/archive-services.blade.php
@@ -6,28 +6,19 @@
   </div>
 
   @php $posts = $fields['title']; @endphp
-    <div class="container content-wrapper">
+    <div class="container">
       @while (have_posts()) @php the_post() @endphp
-      <div class="staff-member member" id="{!! get_the_title() !!}">
+      <div class="staff-member member" id="@php global $post; $post_slug=$post->post_name; echo $post_slug; @endphp">
+
         <div class="row">
-          <div class="col m4" id="bio-pic">
+          <div class="col m3" id="bio-pic">
             <img src="{!! get_the_post_thumbnail_url($id, 'medium') !!}"/>
           </div>
 
-          <div class="col m8" id="bio-info">
+          <div class="col m9" id="bio-info">
             <h3>{!! get_the_title() !!}</h3>
 
-            @if (!empty($description = get_field('description')))
-              {!! $description !!}
-            @endif
-
-            @if (!empty($phone = get_field('phone_number')))
-              <a href="{{$phone}}">{{$phone}}</a>
-            @endif
-
-            @if (!empty($email = get_field('email')))
-              <br><a href="mailto:{{$email}}">{{$email}}</a>
-            @endif
+            @php the_content() @endphp
           </div>
 
         </div>
