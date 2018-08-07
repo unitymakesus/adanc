1c499c32dd89ef330f96930a42000b4592a7d9d1
lexinamer <lexinamer@gmail.com>
2018-08-07 14:03:43 -0400

    trying to compile proper styles for sizing fix and yarn build

--- wp-content/themes/adanc/app/custom-post.php
+++ wp-content/themes/adanc/app/custom-post.php
@@ -2,6 +2,84 @@
 
 namespace App;
 
+// Resources
+function resources_post_type() {
+  register_post_type( 'ada-resource', array(
+    'labels' => array(
+				'name' => 'Resources',
+				'singular_name' => 'Resource',
+				'add_new' => 'Add New',
+				'add_new_item' => 'Add New Resource',
+				'edit' => 'Edit',
+				'edit_item' => 'Edit Resource',
+				'new_item' => 'New Resource',
+				'view_item' => 'View Resource',
+				'search_items' => 'Search Resources',
+				'not_found' =>  'Nothing found in the Database.',
+				'not_found_in_trash' => 'Nothing found in Trash',
+				'parent_item_colon' => ''
+    ),
+    'public' => true,
+    'exclude_from_search' => false,
+    'publicly_queryable' => true,
+    'show_ui' => true,
+    'show_in_nav_menus' => false,
+    'menu_position' => 23,
+    'menu_icon' => 'dashicons-media-document',
+    'capability_type' => 'page',
+    'hierarchical' => false,
+    'supports' => array(
+      'title',
+      'author',
+      'revisions',
+      'page-attributes',
+    ),
+    'has_archive' => false,
+    'rewrite' => array(
+      'slug' => 'resource'
+    )
+  ));
+
+	register_taxonomy('resource-topic',  array('post', 'ada-resource'), array(
+		'labels' => array(
+			'name' => __( 'Topics' ),
+			'singular_name' => __( 'Topic' )
+		),
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_nav_menus' => false,
+		'hierarchical' => true,
+		'rewrite' => true
+	));
+
+  	register_taxonomy('resource-type', 'ada-resource', array(
+  		'labels' => array(
+  			'name' => __( 'Types' ),
+  			'singular_name' => __( 'Type' )
+  		),
+  		'publicly_queryable' => true,
+  		'show_ui' => true,
+  		'show_in_nav_menus' => false,
+  		'hierarchical' => true,
+  		'rewrite' => true
+  	));
+
+	register_taxonomy('resource-source', 'ada-resource', array(
+		'labels' => array(
+			'name' => __( 'Sources' ),
+			'singular_name' => __( 'Source' )
+		),
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_nav_menus' => false,
+		'hierarchical' => true,
+		'rewrite' => true
+	));
+}
+
+add_action( 'init', __NAMESPACE__.'\\resources_post_type' );
+
+
 // Team Post Type
 function staff_post_type() {
 	$labels = array(
