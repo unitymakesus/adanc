1fe8013edf1baf7d249d40ddf3c45c4b4985edb6
lexinamer <lexinamer@gmail.com>
2018-08-20 15:33:49 -0400

    updating resource file

--- wp-content/themes/adanc/resources/views/partials/content-single-resource.blade.php
+++ wp-content/themes/adanc/resources/views/partials/content-single-resource.blade.php
@@ -6,7 +6,7 @@
 @endphp
 
 <div class="resource" itemscope itemtype="http://schema.org/CreativeWork">
-  <h2 class="h3" itemprop="name"><a href="{{ $link }}" target="_blank" rel="noopener" itemprop="url">{{ the_title() }}</a></h2>
+  <h3 class="h3" itemprop="name"><a href="{{ $link }}" target="_blank" rel="noopener" itemprop="url">{{ the_title() }}</a></h3>
 
   @if (!empty($topic_list))
     @php
