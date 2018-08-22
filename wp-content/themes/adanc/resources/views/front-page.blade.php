abc4ec6e21d02eeda6567276f41bb76c890b4b45
lexinamer <lexinamer@gmail.com>
2018-08-22 12:11:03 -0400

    adding search plugin and fixing home blog

--- wp-content/themes/adanc/resources/views/front-page.blade.php
+++ wp-content/themes/adanc/resources/views/front-page.blade.php
@@ -52,17 +52,19 @@
           <div class="col s12 m5 featured-post">
             <h2>Independent Living Blog</h2>
             <p>Keep up-to-date on the latest news and advocacy efforts for the disability community.</p>
+            <div class="post-card">
             <?php $the_query = new WP_Query( 'posts_per_page=1' ); ?>
               <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+                <a href="<?php the_permalink() ?>">
                   <?php if (has_post_thumbnail())
                     the_post_thumbnail( 'medium' ); ?>
                     <h3><?php the_title(); ?></h3>
-                    <?php the_excerpt(__('(more…)')); ?>
-                    <a class="btn" href="<?php the_permalink() ?>">Read More</a>
+                  </a>
               <?php
                 endwhile;
                 wp_reset_postdata();
               ?>
+            </div>
           </div>
           <div class="col s12 m5 offset-m2">
             @if($information)
